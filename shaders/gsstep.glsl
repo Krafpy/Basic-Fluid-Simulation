753f132aac77f91a6e7c2fd39d47bcc7166c8249
Krafpy <nazar.misyats@gmail.com>
2021-05-22 21:19:34 +0200

    Changed .rgb to .xyz in gsstep.

--- shaders/gsstep.glsl
+++ shaders/gsstep.glsl
@@ -13,12 +13,12 @@ vec2 uv(float ox, float oy) {
 }
 
 void main() {
-    vec3 top    = texture2D(field, uv( 0., 1.)).rgb;
-    vec3 bottom = texture2D(field, uv( 0.,-1.)).rgb;
-    vec3 left   = texture2D(field, uv( 1., 0.)).rgb;
-    vec3 right  = texture2D(field, uv(-1., 0.)).rgb;
+    vec3 top    = texture2D(field, uv( 0., 1.)).xyz;
+    vec3 bottom = texture2D(field, uv( 0.,-1.)).xyz;
+    vec3 left   = texture2D(field, uv( 1., 0.)).xyz;
+    vec3 right  = texture2D(field, uv(-1., 0.)).xyz;
 
-    vec3 current = texture2D(field, uv(0., 0.)).rgb;
+    vec3 current = texture2D(field, uv(0., 0.)).xyz;
 
     vec3 new = (current + k*(top + bottom + right + left)) / (1. + 4.*k);
 
